f6963cb79779adfa6b73ba205c5fb771f1790fae
Ghulam Muhammad <ghulammuhammadtunio@gmail.com>
2023-05-27 02:08:44 +0500

    added conditionals for checking any comment-moderations

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-export default ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async (postId) => {
-    console.log("fetching comments");
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(res.data);
-    console.log(comments);
-    console.log(res.data + "for postid = " + postId);
-  };
-
-  useEffect(() => {
-    fetchComments(postId);
-  }, []);
-
-  const renderedComments = comments.map((Comment) => {
-    return <li key={Comment.id}>{Comment.content}</li>;
+import React from "react";
+export default ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected.";
+    } else if (comment.status === "pending") {
+      content = "This comment is awaiting moderation.";
+    } else {
+      content = comment.content;
+    }
+    return <li key={comment.id}>{content}</li>;
   });
-
   return <ul>{renderedComments}</ul>;
 };
